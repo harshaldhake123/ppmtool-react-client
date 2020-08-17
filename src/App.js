6e6bd3a4cb2e660cfe77f3f35b5d366f450ce36f
Harshal Dhake <harshaldhake123@gmail.com>
2020-08-17 17:53:15 +0530

    Bootstrap: Style Dashboard, Navbar, ProjectItem

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
     return (
-        <div className="App">
+        <div className={"App"}>
             <Header/>
             <Dashboard/>
         </div>
